e6beb5383ad0e673c6c8cc16122e9954b7e992c5
Logan <lrl2777@g.rit.edu>
2025-06-09 13:52:31 -0400

    Added base API scripts

---
+++ GGK/Assets/Scripts/API/APIManager.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JetBrains.Annotations;
+using UnityEngine.Networking;
+using UnityEditor.PackageManager.Requests;
+
+// If using Python server nav to the server location and use command: python server.py to start
+
+// OUTDATED - Impliment new logis to connect to server. This will NOR work as of current file setup
+
+public class APIManager : MonoBehaviour
+{
+    private int PID = 0;
+
+    void Start()
+    {
+        StartCoroutine(RunAPIFlow());
+    }
+
+    /// <summary>
+    /// Setting up the post request
+    /// </summary>
+    /// <param name="url">where to send the data</param>
+    /// <param name="jsonData">json data</param>
+    /// <returns></returns>
+    IEnumerator PostJson(string url, string jsonData)
+    {
+        // Sets up the unity request
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        // Sends back each data at a time
+        yield return request.SendWebRequest();
+
+        // Checks the result of teh request 
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Data posted successfully!");
+        }
+        else
+        {
+            Debug.LogError("Error posting data: " + request.error);
+        }
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Requesting and waiting for the desired data
+            yield return webRequest.SendWebRequest();
+
+            // Checking good request
+            if (webRequest.result == UnityWebRequest.Result.Success)
+            {
+                string data = webRequest.downloadHandler.text;
+                PID = int.Parse(data);
+            }
+            else
+            {
+                Debug.LogError("Failed to get data" + webRequest.error);
+            }
+        }
+    }
+
+    IEnumerator RunAPIFlow()
+    {
+        // Sending the request for the PID
+        // Currentally getting data from local
+        // Swap URI and some logic when remote server is up
+        string path = System.IO.Path.Combine(Application.streamingAssetsPath, "testData.txt");
+        Debug.Log("Path: " + path);
+
+        // Chaining the Coroutines so this one finishes before the main one
+        yield return StartCoroutine(GetRequest(path));
+
+        PlayerInfo testPlayer = new PlayerInfo
+        {
+            playerID = PID,
+            raceStartTime = 0330,   // 3:30
+            racePosition = 1,
+            mapRaced = 2,
+            collisionsWithPlayers = 3,
+            collisionWithWalls = 4,
+            characterUsed = 1,
+            boostUsage =
+            {
+                ["Speed Boost 1"] = 5,
+                ["Speed Boost 2"] = 6,
+                ["Speed Boost 3"] = 2
+            },
+            offenceUsage =
+            {
+                ["Puck 1"] = 4,
+                ["Puck 2"] = 2,
+            },
+            trapUsage =
+            {
+                ["Oil spill 1"] = 3,
+                ["Oil spill 2"] = 1
+            }
+        };
+
+        SerializablePlayerInfo serializable = gameObject.GetComponent<SerializablePlayerInfo>();
+        serializable.ConvertToSerializable(testPlayer);
+        string json = JsonUtility.ToJson(serializable);
+
+        StartCoroutine(PostJson("http://127.0.0.1:5000/postdata", json));
+    }
+}
---
+++ GGK/Assets/Scripts/API/SerializablePlayerInfo.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is used to convert the base player info class into serialized data to send over the API
+/// Having this class will slow down the program when selializing the data but will overall 
+/// retain runtime optimization due to the Player data in player into deing in a dictionary
+/// This class is not used for gameplay logic
+/// </summary>
+[System.Serializable]
+public class SerializablePlayerInfo : MonoBehaviour
+{
+    public int playerID, raceStartTime, racePosition, mapRaced, collisionsWithPlayers, collisionWithWalls, characterUsed;
+    public List<ItemUsageEntry> boostUsage = new List<ItemUsageEntry>();
+    public List<ItemUsageEntry> offenceUsage = new List<ItemUsageEntry>();
+    public List<ItemUsageEntry> trapUsage = new List<ItemUsageEntry>();
+
+    public SerializablePlayerInfo ConvertToSerializable(PlayerInfo player)
+    {
+        playerID = player.playerID;
+        raceStartTime = player.raceStartTime;
+        racePosition = player.racePosition;
+        mapRaced = player.mapRaced;
+        collisionsWithPlayers = player.collisionsWithPlayers;
+        collisionWithWalls = player.collisionWithWalls;
+        characterUsed = player.characterUsed;
+
+        // Serializing boost usage dict
+        foreach (KeyValuePair<string, int> kvp in player.boostUsage)
+        {
+            boostUsage.Add(new ItemUsageEntry
+            {
+                itemName = kvp.Key,
+                usageCount = kvp.Value
+            });
+        }
+
+        // Serializing offence usage dict
+        foreach (KeyValuePair<string, int> kvp in player.offenceUsage)
+        {
+            offenceUsage.Add(new ItemUsageEntry
+            {
+                itemName = kvp.Key,
+                usageCount = kvp.Value
+            });
+        }
+
+        // Serializing trap usage dict
+        foreach (KeyValuePair<string, int> kvp in player.trapUsage)
+        {
+            trapUsage.Add(new ItemUsageEntry
+            {
+                itemName = kvp.Key,
+                usageCount = kvp.Value
+            });
+        }
+        return this;
+    }
+}
