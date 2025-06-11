4f1707132f8c5520d6edd54bca2683ce99879193
ceb9515 <143808659+ceb9515@users.noreply.github.com>
2025-06-11 10:06:03 -0400

    Implementation, hopefully with gitignore working

---
+++ GGK/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,41 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_DRIFTSCREECH = 3158137442U;
+        static const AkUniqueID PLAY_ENGINE_HIGH = 3755593723U;
+        static const AkUniqueID PLAY_ENGINE_HIGH_FULL = 1413651457U;
+        static const AkUniqueID PLAY_ENGINE_REV = 1487915922U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
