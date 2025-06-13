ed3a044d25f0008930f02559c34bf7fff4931717
Jayson P <jnp1380@rit.edu>
2025-06-13 10:34:47 -0400

    Arcade Login UI and Login UI scenes created and scripts started

---
+++ GGK/Assets/Scripts/GameManagers/SignInScripts/ArcadeLoginManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcadeLoginManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameManager gameManager;
+
+    private string uIState;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void
+}
