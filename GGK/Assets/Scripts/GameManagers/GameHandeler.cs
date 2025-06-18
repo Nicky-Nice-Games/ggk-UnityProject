866fd894a73f8cd4219bf734fc3752ff26739b97
Jaden35 <jd6030@rit.edu>
2025-06-18 15:37:03 -0400

    Too Bad & HUD text outlined
    players who don't finish the race will see "TOO BAD" on their screen before being sent to the gameover state

--- GGK/Assets/Scripts/GameManagers/GameHandeler.cs
+++ GGK/Assets/Scripts/GameManagers/GameHandeler.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 
 public class GameHandeler : MonoBehaviour
 {
-    [SerializeField]
-    private TextMeshProUGUI timer;
     private GameManager gamemanagerObj;
     private float targetTime = 5.0f;
     private float curTime;
+
+    private float tooBadTime = 8.0f;
+    [SerializeField]
+    private TextMeshProUGUI tooBad;
+
+    [SerializeField]
+    private Image leaderboard;
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +26,28 @@ public class GameHandeler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        curTime = targetTime -= Time.deltaTime;
-        if (targetTime <= 0.0f)
+        if(leaderboard.gameObject.activeSelf)
         {
-            timerEnded();
+            NEWDriver[] karts = GameObject.FindObjectsOfType<NEWDriver>();
+            curTime = targetTime -= Time.deltaTime;
+            if (targetTime <= 0.0f)
+            {
+                curTime += Time.deltaTime;
+                foreach(NEWDriver driver in karts)
+                {
+                    GameObject gameobj = driver.transform.parent.gameObject;
+                    KartCheckpoint kartCheck = gameobj.GetComponentInChildren<KartCheckpoint>();
+                    if(kartCheck.finishTime == 0)
+                    {
+                        tooBad.gameObject.SetActive(true);
+                    }
+                }
+                if (curTime >= tooBadTime)
+                {
+                    timerEnded();
+                }
+            }
         }
-        timer.text = curTime.ToString();
     }
 
     /// <summary>
