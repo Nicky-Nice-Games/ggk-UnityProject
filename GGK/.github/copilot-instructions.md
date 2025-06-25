896a4013fc240df25bbaed2ad46fa3438132e8be
Altera-Sefar <135476626+Altera-Sefar@users.noreply.github.com>
2025-06-25 11:05:18 -0400

    added comments to SignInManager.cs

--- GGK/.github/copilot-instructions.md
+++ GGK/.github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: GGK
 - Unity version: Unity 2022.3.38f1
 - Active game object:
-  - Name: SignInOptionsUI
+  - Name: Sign In Manager
   - Tag: Untagged
-  - Layer: UI
+  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- GGK/Assets/Scripts/LoginScripts/SignInManager.cs
+++ GGK/Assets/Scripts/LoginScripts/SignInManager.cs
@@ -1,3 +1,7 @@
+// Joshua Chisholm
+// 6/25/25
+// Basic button functions for signing in
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +9,8 @@ using UnityEngine.SceneManagement;
 
 public class SignInManager : MonoBehaviour
 {
+
+    // UI elements for different parts of sign in scene
     [SerializeField]
     GameObject loginUI;
     [SerializeField]
@@ -24,17 +30,27 @@ public class SignInManager : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Sets login ui as active and disables login options
+    /// </summary>
     public void ChoseLogin()
     {
         loginOptions.SetActive(false);
         loginUI.SetActive(true);
     }
+
+    /// <summary>
+    /// Sets sign in ui as active and disables login options
+    /// </summary>
     public void ChoseSignUp()
     {
         loginOptions.SetActive(false);
         signUpUI.SetActive(true);
     }
 
+    /// <summary>
+    /// Disables login or signup if they are active and enables login options
+    /// </summary>
     public void BackToSignInOptions()
     {
         loginOptions.SetActive(true);
@@ -42,6 +58,14 @@ public class SignInManager : MonoBehaviour
         loginUI.SetActive(false);
     }
 
+    /// <summary>
+    /// Brings user to start scene as a guest
+    /// 
+    /// NOTE:
+    /// 
+    /// It should be the multisingle scene, but I don't know
+    /// how GameManager works so this is temporary.
+    /// </summary>
     public void ContinueAsGuest()
     {
         SceneManager.LoadScene("StartScene");
