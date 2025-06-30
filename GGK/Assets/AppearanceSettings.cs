9e3aaf7ab3d6c52b6f83367686290e70cad0dea5
Mokko1 <yor7725@g.rit.edu>
2025-06-29 20:04:35 -0400

    Character selection works!
    Added a few new features. One is that you can now see the actual model appear in the Char Selec screen. You can also rotate the model with the kart. When going into a map scene it detects which model it should be having and deletes the others from existing.  To do this I had to edit a few scripts

--- GGK/Assets/AppearanceSettings.cs
+++ GGK/Assets/AppearanceSettings.cs
@@ -7,15 +7,40 @@ public class AppearanceSettings : MonoBehaviour
 {
     public Sprite icon;
     public Color color;
+    public string name;
+    public List<GameObject> models; // List of GameObjects representing different character models
+    CharacterData characterData; // Reference to CharacterData script
     // Start is called before the first frame update
     void Start()
     {
-        //UPDATE ME LATER
+        characterData = FindAnyObjectByType<CharacterData>();
+        name = characterData.characterName;
+        if(characterData != null)
+        {
+            for (int i = 0; i < models.Count; i++)
+                    {
+                        //Setting active correct model
+                        if(name == models[i].name)
+                        {
+                            models[i].SetActive(true);
+                            break;
+                        }
+            
+                        //deleting the models that are not supposed to be active
+                        Destroy(models[i]);
+            
+
+                    }
+        }
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         //UPDATE ME LATER
+
+        
     }
 }
