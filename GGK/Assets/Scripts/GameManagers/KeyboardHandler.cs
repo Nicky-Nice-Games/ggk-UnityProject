07e2f2e88c28c42e083d06e16512ba8ac33ab4da
JakeShapiro1 <jts3681@rit.edu>
2025-07-02 11:53:01 -0400

    worked on controller navigation in menus

--- GGK/Assets/Scripts/GameManagers/KeyboardHandler.cs
+++ GGK/Assets/Scripts/GameManagers/KeyboardHandler.cs
@@ -1,9 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class KeyboardHandler : MonoBehaviour
 {
+    [SerializeField]
+    private EventSystem eventSystem;
+
+    [SerializeField]
+    private Selectable elementToSelect;
+
+
+    private void Reset()
+    {
+        eventSystem = FindObjectOfType<EventSystem>();
+
+        if (eventSystem == null)
+        {
+            Debug.Log("Did not find an Event System in your scene.", context:this);
+        }
+    }
+
+    /// <summary>
+    /// starts at a specified gameobject when changing UI panels
+    /// </summary>
+    public void JumpToElement()
+    {
+        if (eventSystem == null)
+        {
+            Debug.Log("This item has no event system referenced yet", context: this);
+        }
+
+        if (elementToSelect == null)
+        {
+            Debug.Log("This should jump where?", context: this);
+        }
+
+        eventSystem.SetSelectedGameObject(elementToSelect.gameObject);
+    }
+
     // opens controller or keyboard binds
     public void Open()
     {
