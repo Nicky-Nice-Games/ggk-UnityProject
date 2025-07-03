e01d6af152d0f1098277591d74629b688a823081
Gina Piccirilli (RIT Student) <gnp3602@rit.edu>
2025-07-03 14:18:34 -0400

    Starting to Set Up Dev Tools Command Promt

---
+++ GGK/Assets/Scripts/DevTools.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevTools : MonoBehaviour
+{
+
+    public static DevTools Instance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
