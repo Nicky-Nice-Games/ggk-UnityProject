00703d3cd0f78d0b3dfa5882d1f3bd15030be5c6
Mokko1 <yor7725@g.rit.edu>
2025-07-08 11:25:53 -0400

    Finished setting up the script that handles the animator. Also applied textures to Dining model

---
+++ GGK/Assets/TwoDimensionalAnimator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoDimensionalAnimator : MonoBehaviour
+{
+    Animator animator;
+    float turningValue = 0;
+    public NEWDriver driver;
+    public float lerpSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Getting our animator :D
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turningValue = Mathf.Lerp(turningValue, driver.movementDirection.x, Time.deltaTime * lerpSpeed);
+
+
+        //Smoothly interpolating input   
+        animator.SetFloat("turningValue", turningValue);
+    }
+}
