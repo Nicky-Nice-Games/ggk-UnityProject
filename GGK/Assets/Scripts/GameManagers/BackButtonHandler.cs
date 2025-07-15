bb4fcb98347cea82be9d90e460e683778d8653c1
Jaden35 <jd6030@rit.edu>
2025-07-15 16:44:41 -0400

    back button to leave lobbies
    IN PROGRESS: Go Back being set up to have the player leave the lobby if they
    1. are in a lobby
    and
    2. would be going back to the multisingle scene
    
    I also changed the grand prix and time trial stubbed scenes to only let the player go back to the mode select

--- GGK/Assets/Scripts/GameManagers/BackButtonHandler.cs
+++ GGK/Assets/Scripts/GameManagers/BackButtonHandler.cs
@@ -17,8 +17,19 @@ public class BackButtonHandler : MonoBehaviour
 
         // assign back button
         Button button = backOption.GetComponent<Button>();
-        button.onClick.AddListener(() =>
-        GetComponent<BackButtonHandler>().GoBack());
+        
+        // go back (on the game modes scene) will leave the lobby if they are playing multiplayer
+        if (MultiplayerManager.Instance.IsMultiplayer)
+        {
+            Debug.Log("In Multiplayer");
+            button.onClick.AddListener(() =>
+            GetComponent<BackButtonHandler>().LeaveLobby());
+        }
+        else // not multiplayer
+        {
+            button.onClick.AddListener(() =>
+            GetComponent<BackButtonHandler>().GoBack());
+        }
     }
 
     // Update is called once per frame
@@ -44,6 +55,55 @@ public class BackButtonHandler : MonoBehaviour
                 gamemanagerObj.curState = GameStates.multiSingle;
                 break;
             case GameStates.gameMode:
+                gamemanagerObj.sceneLoader.LoadScene("MultiSinglePlayerScene");
+                gamemanagerObj.curState = GameStates.multiSingle;
+                break;
+            case GameStates.playerKart:
+                gamemanagerObj.sceneLoader.LoadScene("GameModeSelectScene");
+                gamemanagerObj.curState = GameStates.gameMode;
+                break;
+            case GameStates.map:
+                gamemanagerObj.sceneLoader.LoadScene("PlayerKartScene");
+                gamemanagerObj.curState = GameStates.playerKart;
+                break;
+            default:
+                break;
+        }
+    }
+
+    // leaves the lobby if you go back from the game mode scene
+    public void LeaveLobby()
+    {
+        switch (gamemanagerObj.curState)
+        {
+            case GameStates.login:
+                gamemanagerObj.sceneLoader.LoadScene("StartScene");
+                gamemanagerObj.curState = GameStates.start;
+                break;
+            case GameStates.multiSingle:
+                gamemanagerObj.sceneLoader.LoadScene("Login");
+                gamemanagerObj.curState = GameStates.login;
+                break;
+            case GameStates.lobby:
+                gamemanagerObj.sceneLoader.LoadScene("MultiSinglePlayerScene");
+                gamemanagerObj.curState = GameStates.multiSingle;
+                break;
+            case GameStates.gameMode:
+                // get the players in the lobby and kick all (except the host)
+                if (LobbyManager.Instance.IsLobbyHost())
+                {
+                    Debug.Log("Kicking");
+                    int players = LobbyManager.Instance.GetJoinedLobby().Players.Count;
+                    for (int i = 0; i < players - 1; i++)
+                    {
+                        LobbyManager.Instance.KickPlayer();
+                    }
+                }
+                // end multiplayer and leave the lobby
+                MultiplayerManager.Instance.IsMultiplayer = false;
+                Debug.Log("Leaving");
+                LobbyManager.Instance.LeaveLobby();
+
                 gamemanagerObj.sceneLoader.LoadScene("MultiSinglePlayerScene");
                 gamemanagerObj.curState = GameStates.multiSingle;
                 break;
