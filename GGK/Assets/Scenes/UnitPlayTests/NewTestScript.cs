6d342b8ac6ac32f12a573f6b40eb9ed3672e6ee0
GiovanniDPaulino <143571398+GiovanniDPaulino@users.noreply.github.com>
2025-07-21 14:57:10 -0400

    Adding assembly definitions for some folders
    Adding assembly definitions to fix an issue with not being able to using certain classes

--- GGK/Assets/Scenes/UnitPlayTests/NewTestScript.cs
+++ GGK/Assets/Scenes/UnitPlayTests/NewTestScript.cs
@@ -20,7 +20,29 @@ public class NewTestScript
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
-        BaseItem baseItem;
+        //BaseItem baseItem;
         yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator MonoBehaviourTest_Works()
+    {
+        yield return new MonoBehaviourTest<MyMonoBehaviourTest>();
+    }
+
+    public class MyMonoBehaviourTest : MonoBehaviour, IMonoBehaviourTest
+    {
+        private int frameCount;
+        private GameObject gameObject = new GameObject();
+        public bool IsTestFinished
+        {
+            get { return frameCount > 10; }
+        }
+
+        void Update()
+        {
+            frameCount++;
+            gameObject.AddComponent<BaseItem>();
+        }
     }
 }
