5dea2d38ab7adbabc772de76cd4d3e6e36524955
dsw039 <d.s.wright2022@gmail.com>
2025-07-22 14:03:24 -0400

    Password can be hidden

---
+++ GGK/Assets/Scripts/LoginScripts/PasswordHider.cs
@@ -0,0 +1,40 @@
+//Daniel Wright
+//7/22/25
+//The purpose of this script is to encrypt the characters being typed into the login password once show password is toggled
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PasswordHider : MonoBehaviour
+{
+    [SerializeField]
+    Toggle showPassword;
+
+    [SerializeField] TMP_InputField passwordInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //When show password is toggled on, the content type is set to standard(the characters are visible)
+        //When it is toggled off, the content type becomes password(makes the characters "*")
+        if (showPassword.isOn)
+        {
+            passwordInput.contentType = TMP_InputField.ContentType.Standard;
+            passwordInput.ForceLabelUpdate();
+        }
+        else
+        {
+            passwordInput.contentType = TMP_InputField.ContentType.Password;
+            passwordInput.ForceLabelUpdate();
+        }
+
+    }
+}
--- GGK/Assets/Scripts/LoginScripts/SignInManager.cs
+++ GGK/Assets/Scripts/LoginScripts/SignInManager.cs
@@ -54,14 +54,14 @@ public class SignInManager : MonoBehaviour
         // Organizing fields list into dict
         foreach (TMP_InputField field in inputFieldsList)
         {
-            inputFields[field.name] = field;
+            inputFields[field.name] = field; 
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+             
     }
 
     /// <summary>
