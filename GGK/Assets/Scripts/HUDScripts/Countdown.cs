c96a8f97cf70be77a32acd73134f7e977a9e0e68
Zasnyy <145603585+Zasnyy@users.noreply.github.com>
2025-07-23 14:21:10 -0400

    Updated Countdown script

--- GGK/Assets/Scripts/HUDScripts/Countdown.cs
+++ GGK/Assets/Scripts/HUDScripts/Countdown.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 
 public class Countdown : NetworkBehaviour
 {
+    /*
     //// List of karts to stop moving while countdown is occuring
     //List<KartCheckpoint> karts;
 
@@ -157,5 +158,125 @@ public class Countdown : NetworkBehaviour
     IEnumerator WaitPlease()
     {
         yield return new WaitForSecondsRealtime(countdownSpeed);
+    }
+    */
+
+    //// List of karts to stop moving while countdown is occuring
+    //List<KartCheckpoint> karts;
+
+    // References
+    public TextMeshProUGUI countdownText;
+
+    // Other
+    public int countdownStart = 3;
+    public float countdownSpeed = 1f;
+    public bool finished = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Pause stuff using scaled time
+        Time.timeScale = 0;
+
+        // Host in Multiplayer 
+        if (IsServer && NetworkManager.Singleton.IsListening)
+        {
+            StartCoroutine(ServerCountdownRoutine());
+        }
+
+        // Singleplayer
+        else if (!NetworkManager.Singleton.IsListening)
+        {
+            StartCoroutine(LocalCountdownRoutine());
+        }
+
+        //// Find karts and turn off their ability to drive
+        //karts = FindObjectsByType<KartCheckpoint>(FindObjectsSortMode.None).ToList();
+
+        //foreach (KartCheckpoint kart in karts)
+        //{
+        //    if (kart.gameObject.GetComponent<NPCDriver>() != null)
+        //    {
+        //        kart.gameObject.GetComponent<NPCDriver>().enabled = false;
+        //    }
+        //    else
+        //    {
+        //        kart.transform.parent.GetChild(0).GetComponent<NEWDriver>().enabled = false;
+        //    }
+        //}
+    }
+
+    IEnumerator ServerCountdownRoutine()
+    {
+        int count = countdownStart;
+
+        while (count > 0)
+        {
+            UpdateCountdownRpc(count);
+            yield return new WaitForSecondsRealtime(countdownSpeed);
+            count--;
+        }
+
+        UpdateCountdownRpc(0); // Show "GO!"
+        EndCountdownRpc();
+        //yield return new WaitForSecondsRealtime(countdownSpeed);
+    }
+
+    IEnumerator LocalCountdownRoutine()
+    {
+        int count = countdownStart;
+
+        while (count > 0)
+        {
+            ShowCountdown(count);
+            yield return new WaitForSecondsRealtime(countdownSpeed);
+            count--;
+        }
+
+        ShowCountdown(0); // Show "GO!"
+        FinishCountdown();
+        //yield return new WaitForSecondsRealtime(countdownSpeed);
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    void UpdateCountdownRpc(int count)
+    {
+        ShowCountdown(count);
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    void EndCountdownRpc()
+    {
+        FinishCountdown();
+    }
+
+    void ShowCountdown(int count)
+    {
+        countdownText.text = count > 0 ? count.ToString() : "GO!";
+
+        countdownText.rectTransform.localScale = Vector3.zero;
+        countdownText.rectTransform.DOScale(Vector3.one, countdownSpeed / 3).SetUpdate(true).OnComplete(() =>
+        {
+            countdownText.rectTransform.DOScale(Vector3.zero, countdownSpeed / 3).SetUpdate(true).SetDelay(countdownSpeed / 3);
+        });
+    }
+
+    void FinishCountdown()
+    {
+        Time.timeScale = 1;
+        finished = true;
+
+        //// turn ability to drive back on
+        //foreach (KartCheckpoint kart in karts)
+        //{
+        //    if (kart.gameObject.GetComponent<NPCDriver>() != null)
+        //    {
+        //        kart.gameObject.GetComponent<NPCDriver>().enabled = true;
+        //    }
+        //    else
+        //    {
+        //        kart.transform.parent.GetChild(0).GetComponent<NEWDriver>().enabled = true;
+        //    }
+        //}
     }
 }
