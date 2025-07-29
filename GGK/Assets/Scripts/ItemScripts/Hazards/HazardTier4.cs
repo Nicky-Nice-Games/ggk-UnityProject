b744758a0cecf22da3b4653ad3d031ec009c35a8
Phillip Brown (RIT Student) <pkb5886@rit.edu>
2025-07-29 14:41:58 -0400

    floating item box functionality added

--- GGK/Assets/Scripts/ItemScripts/Hazards/HazardTier4.cs
+++ GGK/Assets/Scripts/ItemScripts/Hazards/HazardTier4.cs
@@ -12,27 +12,13 @@ public class HazardTier4 : BaseItem
     // Start is called before the first frame update
     void Start()
     {
-
+        timer = 10.0f;
         // sends the hazard slightly up and behind the player before landing on the ground
         // Transform kartTransform = kart.transform; // assuming BaseItem has `kart` reference set by ItemHolder
 
         transform.position = transform.position
                              - transform.forward * 5f   // behind the kart
-                             + transform.up * 1.0f;       // slightly above ground
-        if (itemTier > 1)
-        {
-
-            Renderer cubeRenderer = this.GetComponent<Renderer>();
-            cubeRenderer.material.SetColor("_Color", Color.grey);
-            rb = GetComponent<Rigidbody>();
-            rb.AddForce(new Vector3(0.0f, 100.0f, -750.0f));
-        }
-        else
-        {
-            rb = GetComponent<Rigidbody>();
-            //rBody.AddForce(new Vector3(0.0f, 100.0f, -750.0f));
-            rb.AddForce(transform.forward * -750.0f + transform.up * 50.0f);
-        }
+                             + transform.up * 1.5f;       // slightly above ground
 
     }
 
@@ -40,15 +26,32 @@ public class HazardTier4 : BaseItem
     private new void Update()
     {
         RotateBox();
-    }
-
-    private void FixedUpdate()
-    {
         DecreaseTimer();
     }
 
     public void RotateBox()
     {
         transform.rotation *= new Quaternion(0.0f, 2.0f * Time.deltaTime, 0.0f, 1.0f);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Kart")) // checks if kart gameobject player or npc
+        {
+            Rigidbody kartRigidbody;
+            if (collision.gameObject.TryGetComponent<Rigidbody>(out kartRigidbody)) // checks if they have rb while also assigning if they do
+            {
+                kartRigidbody.velocity *= 0.125f; //this slows a kart down to an eighth of its speed
+
+                if (!MultiplayerManager.Instance.IsMultiplayer)
+                {
+                    Destroy(this.gameObject);
+                }
+                else
+                {
+                    DestroyItemRpc(this);
+                }
+            }
+        }
     }
 }
