e5de849bbfa024021204cebc824de26e00ca83f3
Altera-Sefar <135476626+Altera-Sefar@users.noreply.github.com>
2025-07-31 13:43:36 -0400

    added scene transition for color select

--- GGK/Assets/Scripts/PlayerSelect/ColorSelect.cs
+++ GGK/Assets/Scripts/PlayerSelect/ColorSelect.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ColorSelect : MonoBehaviour
@@ -12,6 +13,7 @@ public class ColorSelect : MonoBehaviour
     // Start is called before the first frame update
 
     private CharacterData charData;
+    private GameManager gameManager;
     [SerializeField]
     private TextMeshProUGUI colorDisplay;
 
@@ -29,6 +31,7 @@ public class ColorSelect : MonoBehaviour
     {
         // Get character data
         charData = FindAnyObjectByType<CharacterData>();
+        gameManager = FindAnyObjectByType<GameManager>();
     }
 
     public void SelectColor(Button color)
@@ -43,6 +46,14 @@ public class ColorSelect : MonoBehaviour
         }
         color.GetComponent<Image>().sprite = buttonSelectedSprite;
         colorDisplay.text = color.name;
+    }
+
+    public void Confirm()
+    {
+        if(charData.characterColor != null)
+        {
+            SceneManager.LoadScene("PlayerKartScene");
+        }
     }
 }
 
