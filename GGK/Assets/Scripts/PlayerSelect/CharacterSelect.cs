815519cb644ac966b41f36c06642cb07376262cb
Altera-Sefar <135476626+Altera-Sefar@users.noreply.github.com>
2025-07-31 14:07:56 -0400

    Basics of character select in with new content

---
+++ GGK/Assets/Scripts/PlayerSelect/CharacterSelect.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> characterButtons;
+
+    [SerializeField]
+    private Sprite buttonDeselected;
+
+    [SerializeField]
+    private Sprite buttonSelectedSprite;
+
+    [SerializeField]
+    private TextMeshProUGUI characterNameDisplay;
+
+    private CharacterData characterData;
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterData = FindObjectOfType<CharacterData>();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectCharacter(Button character)
+    {
+        for (int i = 0; i < characterButtons.Count; i++)
+        {
+            characterButtons[i].GetComponent<Image>().sprite = buttonDeselected;
+        }
+        if (characterData != null)
+        {
+            characterData.characterName = character.name;
+        }
+        character.GetComponent<Image>().sprite = buttonSelectedSprite;
+        characterNameDisplay.text = character.name;
+    }
+}
