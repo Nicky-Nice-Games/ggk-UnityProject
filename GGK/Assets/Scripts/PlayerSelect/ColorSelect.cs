68922b7b14357d7c8782244a613a952e93a699a8
Altera-Sefar <135476626+Altera-Sefar@users.noreply.github.com>
2025-08-04 13:19:28 -0400

    color select set up

---
+++ GGK/Assets/Scripts/PlayerSelect/ColorSelect.cs
@@ -0,0 +1,58 @@
+// 7/31/25
+// Joshua Chisholm
+// Button behavior for color select buttons
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ColorSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private CharacterData charData;
+    private GameManager gameManager;
+    [SerializeField]
+    private TextMeshProUGUI colorDisplay;
+
+    [SerializeField]
+    private Sprite buttonDeselected;
+
+    [SerializeField]
+    private Sprite buttonSelectedSprite;
+
+    [SerializeField]
+    private GameObject buttons;
+
+
+    void Start()
+    {
+        // Get character data
+        charData = FindAnyObjectByType<CharacterData>();
+        gameManager = FindAnyObjectByType<GameManager>();
+    }
+
+    public void SelectColor(Button color)
+    {
+        Button[] colors = buttons.transform.GetComponentsInChildren<Button>();
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i].GetComponent<Image>().sprite = buttonDeselected;
+        }
+        if (charData != null)
+        {
+            charData.characterColor = color.transform.GetChild(0).GetComponent<Image>().color;
+        }
+        color.GetComponent<Image>().sprite = buttonSelectedSprite;
+        colorDisplay.text = color.name;
+    }
+
+    public void Confirm()
+    {
+        if (charData.characterColor != null)
+        {
+            SceneManager.LoadScene("CharSelectMenu");
+        }
+    }
+}
