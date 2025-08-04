0655fce4342c85ddf3a5b26738f48f04a233acc9
Altera-Sefar <135476626+Altera-Sefar@users.noreply.github.com>
2025-08-04 13:34:08 -0400

    char select scene hooked up
    no back button yet

---
+++ GGK/Assets/Scripts/PlayerSelect/CharacterSelect.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject buttons;
+
+    [SerializeField]
+    private Sprite buttonDeselected;
+
+    [SerializeField]
+    private Sprite buttonSelectedSprite;
+
+    [SerializeField]
+    private TextMeshProUGUI characterNameDisplay;
+
+    [SerializeField]
+    private List<GameObject> characterModels;
+
+    private CharacterData characterData;
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterData = FindObjectOfType<CharacterData>();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SelectCharacter(Button character)
+    {
+        Button[] characters = buttons.GetComponentsInChildren<Button>();
+        for (int i = 0; i < characters.Length; i++)
+        {
+            characters[i].GetComponent<Image>().sprite = buttonDeselected;
+        }
+
+        // for (int i = 0; i < characterModels.Count; i++)
+        // {
+        //     if (characterModels[i].name == character.name)
+        //     {
+        //         characterModels[i].SetActive(true);
+        //     }
+        //     else
+        //     {
+        //         characterModels[i].SetActive(false);
+        //     }
+        // }
+
+        if (characterData != null)
+        {
+            characterData.characterName = character.name;
+            characterData.characterSprite = character.gameObject.transform.GetChild(0).GetComponent<Image>().sprite;
+        }
+        character.GetComponent<Image>().sprite = buttonSelectedSprite;
+        characterNameDisplay.text = character.name;
+    }
+
+    public void Confirm()
+    {
+        if (characterData.characterName != null)
+        {
+            SceneManager.LoadScene("MapSelectScene");
+        }
+    }
+}
