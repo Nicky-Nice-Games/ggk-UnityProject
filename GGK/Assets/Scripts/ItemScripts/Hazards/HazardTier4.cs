3324f7c6c831051c35306cd756a60a85f6ae3f51
JakeShapiro1 <jts3681@rit.edu>
2025-08-11 13:27:35 -0400

    added cloud to blizzard

--- GGK/Assets/Scripts/ItemScripts/Hazards/HazardTier4.cs
+++ GGK/Assets/Scripts/ItemScripts/Hazards/HazardTier4.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class HazardTier4 : BaseItem
 {
+    GameObject hazardVFX;
+
     private void Start()
     {
         Vector3 behindPos = transform.position - transform.forward * 11;
         behindPos.y += 0.8f;
         transform.position = behindPos;
         kart.GetComponent<NEWDriver>().playerInfo.trapUsage["fakepowerupbrick"]++;
+        hazardVFX = GameObject.Find("VFX");
     }
 
     private new void Update()
@@ -20,7 +23,7 @@ public class HazardTier4 : BaseItem
 
     private void RotateBox()
     {
-        transform.rotation *= new Quaternion(0.0f, 1.5f * Time.deltaTime, 0.0f, 1.0f);
+        hazardVFX.transform.rotation *= new Quaternion(0.0f, 1.5f * Time.deltaTime, 0.0f, 1.0f);
     }
 
     private void OnTriggerEnter(Collider collision)
