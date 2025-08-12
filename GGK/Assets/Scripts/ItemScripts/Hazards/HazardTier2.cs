9a1d54294b7c7e835d5dee5ef6dbfcbd5d9472c8
Elias Duke <egd4546@rit.edu>
2025-08-12 13:57:21 -0400

    Added meshrenderer serialized field to the hazard tier 2

--- GGK/Assets/Scripts/ItemScripts/Hazards/HazardTier2.cs
+++ GGK/Assets/Scripts/ItemScripts/Hazards/HazardTier2.cs
@@ -9,6 +9,9 @@ using UnityEngine.UIElements;
 /// </summary>
 public class HazardTier2 : BaseItem
 {
+
+    [SerializeField] private MeshRenderer meshRenderer; // The fake item brick's mesh renderer
+
     // Start is called before the first frame update
     void Start()
     {
